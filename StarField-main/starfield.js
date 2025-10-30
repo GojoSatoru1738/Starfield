d906ff0e501b62b3dbd77979c49c2562c8a75283
GojoSatoru1738 <ajmiller@email.neit.edu>
2025-10-30 10:30:57 -0400

    YEAHHHH BUDDY 2

--- StarField-main/starfield.js
+++ StarField-main/starfield.js
@@ -3,7 +3,13 @@ let pencil = canvas.getContext("2d");
 
 
 function gameLoop() {
-    
+
+    //draw backround
+    //draw stars
+    //move stars
+    //recycle stars
+
+
 }
 
 setInterval(gameLoop, 10);
